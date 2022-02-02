2a7701d33071c952fff3b318e9ca1044cd017a0c
TairovichJR <yokubov.omonjon@gmail.com>
2022-02-01 21:11:50 -0500

    parallel execution on amazon.google yahoo

--- src/test/java/ParallelExecutionBrowser.java
+++ src/test/java/ParallelExecutionBrowser.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -16,24 +17,16 @@ import java.time.Instant;
 public class ParallelExecutionBrowser {
 
     static final String searchItem = "java for beginners";
-    long start = 0;
-    long end = 0;
+    WebDriver driver;
 
-    @BeforeTest
-    public void start(){
-        start = Instant.now().getEpochSecond();
-    }
-
-    @AfterTest
-    public void end(){
-        end = Instant.now().getEpochSecond();
-        System.out.println(end - start);
+    @BeforeMethod
+    public void setUP(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
     @Test
     public void googleTest(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
         driver.get("https://google.com");
         driver.findElement(By.name("q")).sendKeys(searchItem + Keys.ENTER);
@@ -42,8 +35,7 @@ public class ParallelExecutionBrowser {
 
     @Test
     public void amazonTest(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
         driver.get("https://amazon.com");
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys(searchItem + Keys.ENTER);
@@ -51,8 +43,7 @@ public class ParallelExecutionBrowser {
 
     @Test
     public void yahooTest(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
         driver.get("https://yahoo.com");
         driver.findElement(By.id("ybar-sbq")).sendKeys(searchItem + Keys.ENTER);
